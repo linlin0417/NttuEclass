11c8a24465219a5a586d36bbef3251cb40727664
linlin <linlin0417022@gmail.com>
2026-09-09 17:05:06 +0800

    chore(release): bump version to 1.0.5 (10006)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "tw.edu.irika.nttueclass"
         minSdk = 31
         targetSdk = 36
-        versionCode = 10005
-        versionName = "1.0.4"
+        versionCode = 10006
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
